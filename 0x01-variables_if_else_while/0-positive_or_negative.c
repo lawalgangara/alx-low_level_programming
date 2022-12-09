5edd3b19ac1943785a73da65eb29664f0c6bb622
lawalgangara <law4zul@gmail.com>
2022-12-09 15:35:31 -0800

    task zero

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+/* betty style doc for funtion main goes there */
+int main(void)
+{ 
+	int n;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* YOUR CODE GOES THERE */
+	return (0);
+}
