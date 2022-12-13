321b54acce56bf2d4ee049fdfdd1ae1b2a16a2f0
lawalgangara <law4zul@gmail.com>
2022-12-13 07:20:24 -0800

    Taskl_0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,7 +6,7 @@
  * return: 0
  */
 
-int main(void)
+int void  main(void)
 {
 	_putchar('_');
 	_putchar('p');
@@ -16,6 +16,6 @@ int main(void)
 	_putchar('h');
 	_putchar('a');
 	_putchar('r');
-	_putchar('\n')
+	_putchar('\n');
 	return (0);
 }
