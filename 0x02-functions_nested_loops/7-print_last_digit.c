627a22cb9ab14c615401b8a4dbc5f2e0a66b5954
lawalgangara <law4zul@gmail.com>
2022-12-13 12:30:02 -0800

    Task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_last_digit - print the last digit of a num
+ * @n: to print the value
+ * Return: always 0
+ */
+int print_last_digit(int n)
+{
+	int lastdigit = 10;
+	char letter;
+
+	while (lastdigir-- <= 10)
+	{
+		for (letter = 'a'; letter <= 'z'; letter--)
+			-putchar(letter);
+		return (0);
