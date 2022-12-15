8e2f8744ad8ece0dec36db402099cf73cd7096c8
lawalgangara <law4zul@gmail.com>
2022-12-15 04:31:00 -0800

    Task 0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,12 @@
+# include "main.h"
+/**
+ * main - tests funtion that print if integer is positive or negative.
+ * Return: 0
+ */
+int main(void)
+{
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
