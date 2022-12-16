ab829926cc7852175847ebf67886f202dcb21b0b
lawalgangara <law4zul@gmail.com>
2022-12-16 13:16:58 -0800

    Task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - print all single digit
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+		putchar((num % 10) + '0');
+	putchar('\n');
+	return (0);
+}
