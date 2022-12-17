1045866f1c65403d8086560938d74b31b412f880
lawalgangara <law4zul@gmail.com>
2022-12-17 05:32:06 -0800

    Task 11

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,21 +11,26 @@
 
 int main(void)
 {
-	int i;
+	i = 0;
 
-	for (i = 1; i <= 100; i++)
+	for (unsigned int i = 1; i <= 100; i++)
 	{
-		if (i % 15 == 0)
-			printf("FizzBuzz");
-		else if (i % 3 == 0)
-			printf("Fizz");
-		else if (i % 5 == 0)
-			printf("Buzz");
-		else
-			printf("%i", i);
-		if (i < 100)
-			printf(" ");
-	}
-	printf("\n");
-	return (0);
+		 if (i % 15 == 0)
+		 {
+			 printf("FizzBuzz\n");
+		 }
+		 else if (i % 3 == 0)
+		 {
+			 printf("Fizz\n");
+		 }
+		 else if (i % 5 == 0)
+		 {
+			  printf("Buzz\n");
+		 }
+		 else
+		 {
+			 printf("%u\n", i);
+		 }
+	 }
+		 return (0);
 }
