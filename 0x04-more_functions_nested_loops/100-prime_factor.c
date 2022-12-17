b829819584c90a2b0c4bf16a35a8ede20c23c977
lawalgangara <law4zul@gmail.com>
2022-12-17 06:09:01 -0800

    Task 11

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -5,26 +5,15 @@
  */
 int main(void)
 {
-	int d1, d2, d3;
+	unsigned long int prime = 612852475143;
+	unsigned long int i = 3;
 
-	for (d1 = 0; d1 < 8; d1++)
-	{
-		for (d2 = d1 + 1; d2 < 9; d2++)
-		{
-			for (d3 = d1 + 1; d3 < 10; d3++)
-			{
-				putchar((d1 % 10) + '0');
-				putchar((d2 % 10) + '0');
-				putchar((d3 % 10) + '0');
+	while (i <= prime / 2)
+		if (prime % i == 0)
+			prime = prime / i;
+		else
+			i += 2;
+	printf("%ld\n", prime);
 
-				if (d1 == 7 && d2 == 8 && d3 == 9)
-					continue;
-
-				putchar(',');
-				putchar(' ');
-			}
-		}
-	}
-	putchar('\n');
 	return (0);
 }
