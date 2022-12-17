1808196869fa1fbc324077c8bcda29fe8dfd4880
lawalgangara <law4zul@gmail.com>
2022-12-17 08:28:12 -0800

    Task 12

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,7 +7,7 @@
 void print_number(int n)
 {
 	unsigned int num = n;
-	{
+	
 
 	if (n < 0)
 	{
@@ -17,7 +17,6 @@ void print_number(int n)
 	if ((num / 10) > 0)
 		print_number(num / 10);
 	putchar((num % 10) + '0');
-	}
-	putchar('\n');
-	return (0);
+	
+	
 }
