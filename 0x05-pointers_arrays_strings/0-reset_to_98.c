d6dc2d4826a8c49861c1f2a0170d12952926dfaa
lawalgangara <law4zul@gmail.com>
2022-12-19 06:36:19 -0800

    Task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - a function that takes a pointer to an int
+ * @n: integer to update
+ * Return: Always 0.
+ *
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
