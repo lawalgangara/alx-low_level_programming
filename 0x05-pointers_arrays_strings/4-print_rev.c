08414e2674f8569fe762c73a0a3bfd78ec2a3089
lawalgangara <law4zul@gmail.com>
2022-12-20 04:49:48 -0800

    Task 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,14 +7,13 @@
  */
 void print_rev(char *s)
 {
-	int a;
-	int b;
+	int a, b;
 
 	b = 0;
 
-	while (*s[b] != '\0')
+	while (s[b] != '\0')
 		b++;
-	for (a = b -1; a > = 0; a--)
+	for (a = b - 1; a > = 0; a--)
 	{
 		_putchar(s[a]);
 	}
