e75bb598443561770be13acd2f43717062d2b4c4
lawalgangara <law4zul@gmail.com>
2023-01-03 12:55:45 -0800

    Task 0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -12,11 +12,13 @@
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i = 0;
+
+	char *start = s;
 	
 	while (i < n)
 	{
-		s[i] = b;
+		*s++ = b;
 		i++;
 	}
-	return (s);
+	return (start);
 }
