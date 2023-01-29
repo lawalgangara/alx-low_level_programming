a1d591711a4c0bf0224e87db0758deb10586e30b
lawalgangara <law4zul@gmail.com>
2023-01-29 14:55:31 -0800

    Task 3

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -20,6 +20,7 @@ unsigned int _strlen(char *str)
  * add_node_end - adds a new node to the end of linked list
  * @head: double pointer to a linked list
  * @str: string to add to the new nodeReturn: pointer to the new node
+ * Return: 0 always.
  */
 list_t *add_node_end(list_t **head, const char *str)
 
